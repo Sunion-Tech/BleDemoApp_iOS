1f25359e0c07cda177d2a6d55a0d927d6b0dbabc
Cthiisway <bing_chicho@sunion.com.tw>
2024-03-06 09:15:46 +0800

    init

---
+++ BleDemoApp_iOS/BleDemoApp_iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BleDemoApp_iOS
+//
+//  Created by Cthiisway on 2024/3/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
